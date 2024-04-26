7530c83362d9c7983257272c84b1786882ed7ba7
Erick <Edck03030303@outlook.com>
2024-04-25 22:11:48 -0300

    commit inicial projeto loja virtual

---
+++ src/main/java/loja_virtual_TysBy/LojaTysByApplication.java
@@ -0,0 +1,13 @@
+package loja_virtual_TysBy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaTysByApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaTysByApplication.class, args);
+	}
+
+}
---
+++ src/test/java/loja_virtual_TysBy/LojaTysByApplicationTests.java
@@ -0,0 +1,13 @@
+package loja_virtual_TysBy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaTysByApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
